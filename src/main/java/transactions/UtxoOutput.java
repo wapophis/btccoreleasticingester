3cc31259ba54a32401d81223b3d6f7445b6265d4
wapophis <atlialogic@gmail.com>
2021-02-08 22:19:16 +0100

    Added uuid to the utxo ouput, to track the full three transactions between vin and vout

--- src/main/java/transactions/UtxoOutput.java
+++ src/main/java/transactions/UtxoOutput.java
@@ -1,11 +1,10 @@
 package transactions;
 
-import java.util.ArrayList;
+import java.util.UUID;
 
 public class UtxoOutput{
     public Double value;
     public int n;
     public ScriptPubKey scriptPubKey;
-
-
+    public String uid=UUID.randomUUID().toString();
 }
