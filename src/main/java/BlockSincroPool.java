1a174c1e680e74b103e1f356225a5c3590007ae8
wapophis <atlialogic@gmail.com>
2021-02-09 10:17:03 +0100

    BlockSincroPool is failing in some txs. Now make it runnable to be threaded by the launcher.

--- src/main/java/BlockSincroPool.java
+++ src/main/java/BlockSincroPool.java
@@ -1,7 +1,6 @@
 import com.google.gson.Gson;
 import daemon.ConsoleColors;
 import daemon.Params;
-import org.apache.commons.collections4.queue.CircularFifoQueue;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.bulk.*;
 import org.elasticsearch.action.index.IndexRequest;
@@ -19,42 +18,37 @@ import threading.TransacionUtxoDecoder;
 import transactions.BtcTransaction;
 import transactions.UtxoTuple;
 
-import java.io.IOException;
 import java.math.BigDecimal;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 /**
  * This class sincronize with the elastic search index the utxos xtracted from the btc core node
  */
-public class BlockSincroPool {
+public class BlockSincroPool implements Runnable {
     private final static int MAX_TO_SINCRO=100;
-    private static CircularFifoQueue<BtcTransaction> queue = new CircularFifoQueue<>(MAX_TO_SINCRO);
+
     private final static int BATCH_SIZE=500;
-    private static final ConcurrentLinkedQueue<UtxoTuple> utxos=new ConcurrentLinkedQueue<>();
+
     private static SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+
     private static Gson gson=new Gson();
 
-    private static final ExecutorService executor = Executors.newFixedThreadPool(8);//creating a pool of 5 threads
-
     private static HttpHost[] esHosts=new HttpHost[Params.elasticSearchServerUri.size()];
+
     static{
         for(int i=0;i<Params.elasticSearchServerUri.size();i++){
             esHosts[i]=new HttpHost(Params.elasticSearchServerUri.get(i));
         }
     }
-    private static final RestHighLevelClient highLevelClient = new RestHighLevelClient(
-            RestClient.builder(esHosts));
 
+    private static final RestHighLevelClient highLevelClient = new RestHighLevelClient(RestClient.builder(esHosts));
 
     /*** Generic listener for bulkprocessor*/
     private static BulkProcessor.Listener listener = new BulkProcessor.Listener() {
@@ -77,113 +71,124 @@ public class BlockSincroPool {
         }
     };
 
+    private static BulkRequest request = new BulkRequest();
 
-        private static BulkRequest request = new BulkRequest();
+    private ConcurrentLinkedQueue<UtxoTuple> btcUtxos;
+    private BtcTransaction transaction;
+    private Date registringDate;
+    private BigDecimal spotPrice;
 
-
-    public static void  addToSincroPool(BtcTransaction transaction,Date registeringDate,BigDecimal spotprice) throws IOException, ParseException {
-        if(queue.isFull()){
-            throw new IllegalStateException("POOL IS FULL, TX OVERFLOW:"+transaction.txid);
-        }
-        queue.add(transaction);
-        onAdded(registeringDate,spotprice);
+    public BlockSincroPool(BtcTransaction transaction,Date registringDate,BigDecimal spotPrice){
+        this.transaction=transaction;
+        this.registringDate=registringDate;
+        this.spotPrice=spotPrice;
+        this.btcUtxos=new ConcurrentLinkedQueue<>();
     }
 
-
-    private static void onAdded(Date registeringDate, BigDecimal spotPrice) throws IOException, ParseException {
-        // UTXOS ARE ADDED
-        if(utxos.size()>=BATCH_SIZE){
-
-
-
-            BulkProcessor.Builder builder = BulkProcessor.builder(
-                    (request, bulkListener) ->
-                            highLevelClient.bulkAsync(request, RequestOptions.DEFAULT, bulkListener),
-                    listener);
-            builder.setBulkActions(BATCH_SIZE);
-            builder.setBulkSize(new ByteSizeValue(1L, ByteSizeUnit.MB));
-            builder.setConcurrentRequests(1);
-            builder.setFlushInterval(TimeValue.timeValueSeconds(10L));
-            builder.setBackoffPolicy(BackoffPolicy
-                    .constantBackoff(TimeValue.timeValueSeconds(1L), 3));
-            BulkProcessor bulkProcessor=builder.build();
-
-            utxos.forEach(new Consumer<UtxoTuple>() {
-                @Override
-                public void accept(UtxoTuple utxoTuple) {
-                    if(utxoTuple.spent){
-                        /// UPDATES
-                        Map<String,Object> parameters=new HashMap<>();
-
-                        parameters.put("spendPrice",utxoTuple.spent_price);
-                        parameters.put("spendDate",utxoTuple.spentDate);
-                        parameters.put("spentValue",true);
-
-                        Script script= new Script(ScriptType.STORED,null,"spendUtxo",parameters);
-
-                        bulkProcessor.add(new UpdateRequest(Params.utxoIndexName,utxoTuple.txid + "$" + utxoTuple.n).id(utxoTuple.txid + "$" + utxoTuple.n)
-                                .script(script));
-
-                    }else{
-                        // NEW UTXOS
-                        bulkProcessor.add(new IndexRequest(Params.utxoIndexName).id(utxoTuple.txid + "$" + utxoTuple.n)
-                                .source(gson.toJson(utxoTuple), XContentType.JSON));
-                    }
-                    utxos.remove(utxoTuple);
-                }
-            });
-
-            try {
-           //     System.out.print(ConsoleColors.GREEN+" Terminated client "+ConsoleColors.RESET);
-                boolean terminated = bulkProcessor.awaitClose(30L, TimeUnit.SECONDS);
-           //     System.out.print(" "+terminated+" >>> | ");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }else {
-
-            executor.execute(new TransacionUtxoDecoder(queue.poll(), utxos, sdf.format(registeringDate),spotPrice));
-        }
-
-    }
-
-
-    public static  void processBulkResponse(BulkResponse bulkItemResponses){
+    private static void processBulkResponse(BulkResponse bulkItemResponses){
 
         HashMap<String,Integer> failures=new HashMap<>();
+
         HashMap<String,Integer> operations=new HashMap<>();
-        System.out.print("BULK RESPONSE:"+bulkItemResponses.getTook()+" - "+
-                    bulkItemResponses.getItems().length+" - ");
+
+        System.out.print("BULK RESPONSE:"+bulkItemResponses.getTook()+" - "+bulkItemResponses.getItems().length+" - ");
 
                         Arrays.asList(bulkItemResponses.getItems()).forEach(new Consumer<BulkItemResponse>() {
                             @Override
                             public void accept(BulkItemResponse bulkItemResponse) {
                                 if(operations.get(bulkItemResponse.getOpType().getLowercase())!=null){
+
                                     int count=operations.get(bulkItemResponse.getOpType().getLowercase());
+
                                     count++;
+
                                     operations.put(bulkItemResponse.getOpType().getLowercase(),count);
+
                                 }else{
+
                                     operations.put(bulkItemResponse.getOpType().getLowercase(),0);
                                 }
 
-
                                 if(bulkItemResponse.isFailed()){
                                     Integer count=failures.get(bulkItemResponse.getFailure().getType());
+
                                     if(count==null){
+
                                         count=0;
                                     }
+
                                     count++;
+
                                     failures.put(bulkItemResponse.getOpType().toString(),count);
                                 }
                             }
                         });
+
                         System.out.print(ConsoleColors.GREEN+" BULK PROCESSING STATS "+gson.toJson(operations)+ConsoleColors.RESET);
+
                         System.out.print(ConsoleColors.GREEN+" BULK PROCESSING FAILURES DETECTED "+gson.toJson(failures)+ConsoleColors.RESET);
                         //System.out.print(" \r");
     }
 
+    @Override
+    public void run() {
+        new TransacionUtxoDecoder(transaction, this.btcUtxos, sdf.format(this.registringDate),this.spotPrice).run();
+        sendToEsServer();
 
+    }
 
+    private void sendToEsServer()
+    {
+        BulkProcessor.Builder builder = BulkProcessor.builder(
+                (request, bulkListener) ->
+                        highLevelClient.bulkAsync(request, RequestOptions.DEFAULT, bulkListener),
+                listener);
 
+        builder.setBulkActions(BATCH_SIZE);
 
+        builder.setBulkSize(new ByteSizeValue(1L, ByteSizeUnit.MB));
+
+        builder.setConcurrentRequests(1);
+
+        builder.setFlushInterval(TimeValue.timeValueSeconds(10L));
+
+        builder.setBackoffPolicy(BackoffPolicy
+                .constantBackoff(TimeValue.timeValueSeconds(1L), 3));
+
+        BulkProcessor bulkProcessor=builder.build();
+
+        btcUtxos.forEach(new Consumer<UtxoTuple>() {
+            @Override
+            public void accept(UtxoTuple utxoTuple) {
+                if(utxoTuple.spent){
+                    /// UPDATES
+                    Map<String,Object> parameters=new HashMap<>();
+
+                    parameters.put("spendPrice",utxoTuple.spent_price);
+
+                    parameters.put("spendDate",utxoTuple.spentDate);
+
+                    parameters.put("spentValue",true);
+
+                    Script script= new Script(ScriptType.STORED,null,"spendUtxo",parameters);
+
+                    bulkProcessor.add(new UpdateRequest(Params.utxoIndexName,utxoTuple.txid + "$" + utxoTuple.n).id(utxoTuple.txid + "$" + utxoTuple.n)
+                            .script(script));
+
+                }else{
+                    // NEW UTXOS
+                    bulkProcessor.add(new IndexRequest(Params.utxoIndexName).id(utxoTuple.txid + "$" + utxoTuple.n)
+                            .source(gson.toJson(utxoTuple), XContentType.JSON));
+                }
+            }
+        });
+
+        try {
+            //     System.out.print(ConsoleColors.GREEN+" Terminated client "+ConsoleColors.RESET);
+            boolean terminated = bulkProcessor.awaitClose(30L, TimeUnit.SECONDS);
+            //     System.out.print(" "+terminated+" >>> | ");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
